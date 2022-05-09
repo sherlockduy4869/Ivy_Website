e5cb41590c06adfcb6f3000812d9ee4b130e6c2f
Duy-Kun <sherlockduy2001@gmail.com>
2022-05-09 22:55:07 +0700

    Done product-details

--- Resource/js/script.js
+++ Resource/js/script.js
@@ -15,46 +15,51 @@ $(document).ready(function(){
     )
 
     //Slider
-    const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
-    const imgContainer = document.querySelector(".aspect-ratio-169")
-    let index = 0
-    let imgNumber = imgPosition.length
-    const dotItem = document.querySelectorAll(".dot")
+    const imgPosition = document.querySelectorAll(".aspect-ratio-169 img");
+    const imgContainer = document.querySelector(".aspect-ratio-169");
+    let index = 0;
+    let imgNumber = imgPosition.length;
+    const dotItem = document.querySelectorAll(".dot");
     //console.log(imgPosition)
     //abc
     imgPosition.forEach(function(image, index){
         image.style.left = index*100 + "%"
         dotItem[index].addEventListener("click",function(){
-        slider(index)    
+        slider(index);
         })
     })
 
     function imgSlide(){
         index++;
         if(index>=imgNumber){
-            index = 0
+            index = 0;
         }
-        slider(index)
+        slider(index);
     }   
 
     function slider(index){
-        imgContainer.style.left = "-" +index*100+ "%"
-        const dotActive = document.querySelector(".active")
-        dotActive.classList.remove("active")
-        dotItem[index].classList.add("active")
+        imgContainer.style.left = "-" +index*100+ "%";
+        const dotActive = document.querySelector(".active");
+        dotActive.classList.remove("active");
+        dotItem[index].classList.add("active");
     }
     setInterval(imgSlide,5000)
 
     const itemSliderbar = document.querySelectorAll(".category-left-li")
     itemSliderbar.forEach(function(menu,index){
         menu.addEventListener("click",function(){
-            menu.classList.toggle("block")
+            menu.classList.toggle("block");
         })
         
     })
 
-
-
-
-
+    //Product details
+    
+    const bigImg = document.querySelector(".big-image");
+    const smallImg = document.querySelectorAll(".small-image");
+    smallImg.forEach(function(imgItem){
+        imgItem.addEventListener("click",function(){
+            bigImg.src = imgItem.src;
+        })
+    })
 })
