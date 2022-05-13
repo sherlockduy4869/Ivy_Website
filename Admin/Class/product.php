dbebe2fe4a82867d80a16f9a8c907efcddb15e70
Duy-Kun <sherlockduy2001@gmail.com>
2022-05-13 08:44:23 +0700

    Create product class and showing cateList in productadd

---
+++ Admin/Class/product.php
@@ -0,0 +1,79 @@
+<?php
+    include "Lib/database.php";
+    include "Helpers/format.php";
+?>
+
+<?php
+
+    class product
+    {
+        private $db ;
+        private $fm ;
+
+        public function __construct()
+        {
+            $this->db = new Database();
+            $this->fm = new Format();
+        }
+
+
+
+
+    //     public function insert_category($cateName){
+
+    //         $cateName = $this->fm->validation($cateName);
+    //         $cateName = mysqli_real_escape_string($this->db->link, $cateName);
+
+    //         $query = "INSERT INTO tbl_category(cateName) VALUES('$cateName')";
+    //         $result = $this->db->insert($query);
+
+    //         if($result)
+    //         {
+    //             $alert = "<span class = 'addSuccess'>Add category successfully</span>";
+    //             return $alert;
+    //         }
+    //         else{
+    //             $alert = "<span class = 'addError'>Can not add category</span>";
+    //             return $alert;
+    //         }
+    //     }
+
+    //     public function show_category_list(){
+    //         $query = "SELECT * FROM tbl_category ORDER BY cateID DESC";
+    //         $result = $this->db->insert($query);
+    //         return $result;
+    //     }
+        
+    //     public function get_cate_name_by_id($cateID){
+    //         $query = "SELECT * FROM tbl_category WHERE cateID = '$cateID'";
+    //         $result = $this->db->select($query);
+    //         return $result;
+    //     }
+
+    //     public function edit_category($cateID, $cateName){
+
+    //         $cateName = $this->fm->validation($cateName);
+    //         $cateName = mysqli_real_escape_string($this->db->link, $cateName);
+
+    //         $query = "UPDATE tbl_category SET cateName = '$cateName' WHERE cateID = '$cateID'";
+    //         $result = $this->db->update($query);
+    //         if($result)
+    //         {
+    //             $alert = "<span class = 'addSuccess'>Edit category successfully</span>";
+    //             return $alert;
+    //         }
+    //         else{
+    //             $alert = "<span class = 'addError'>Can not edit category</span>";
+    //             return $alert;
+    //         }
+    //     }
+
+    //     public function delete_category($delID){
+    //         $query = "DELETE FROM tbl_category WHERE cateID = '$delID'";
+    //         $result = $this->db->delete($query);
+
+    //         header('Location:categorylist.php');
+    //     }
+    }
+
+?>
--- Admin/productadd.php
+++ Admin/productadd.php
@@ -1,30 +1,48 @@
 <?php
     include 'Include/header.php';
     include 'Include/slider.php';
+    include 'Class/category.php';
 
+?>
+
+<?php
+    $cate = new category();
+    $cateList = $cate->show_category_list();
 ?>
     <div class="admin-content-right">
             <div class="admin-content-right-product_add">
                 <h1>Add product</h1>
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="productadd.php" method="POST" enctype="multipart/form-data">
                     <label for="">Enter product name<span style="color: red;">*</span></label>
-                    <input type="text">
+                    <input type="text" name="productName" required>
                     <label for="">Choose category<span style="color: red;">*</span></label>
                     <select name="" id="">
                         <option value="">--Category--</option>
+                        <?php
+                            if(isset($cateList))
+                            {
+                                while($result = $cateList->fetch_assoc())
+                                {            
+                                
+                        ?>
+                        <option value=""><?php echo $result['cateName']; ?></option>
+                        <?php
+                                }
+                            }
+                        ?>
                     </select>
                     <label for="">Product price<span style="color: red;">*</span></label>
-                    <input type="text">
+                    <input type="text" required>
                     <label for="">Product description<span style="color: red;">*</span></label>
-                    <textarea name="editor" id="" cols="30" rows="10"></textarea>
+                    <textarea name="editor" id="" cols="30" rows="10" required></textarea>
                     <label for="">Choose type<span style="color: red;">*</span></label>
                     <select name="" id="">
                         <option value="">--Type--</option>
                     </select>
                     <label for="">Product picture<span style="color: red;">*</span></label>
-                    <input type="file">
+                    <input type="file" required>
                     <label for="">Product picture description<span style="color: red;">*</span></label>
-                    <input type="file" multiple>
+                    <input type="file" multiple required>
                     <button type="submit">Add</button>
                 </form>
             </div>
