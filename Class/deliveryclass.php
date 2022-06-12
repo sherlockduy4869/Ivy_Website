5398537fe082a8f86a68796ab54641ec9dfdae3c
Duy-Kun <sherlockduy2001@gmail.com>
2022-06-12 15:47:36 +0700

    Do adding customer information in delivery page

--- Class/deliveryclass.php
+++ Class/deliveryclass.php
@@ -17,61 +17,19 @@
         }
 
         //Insert product
-        public function insert_product($data, $files){
+        public function insert_customer_information($data){
 
-            $product_name = mysqli_real_escape_string($this->db->link, $data['product_name']);
-            $category_id = mysqli_real_escape_string($this->db->link, $data['category_id']);
-            $price = mysqli_real_escape_string($this->db->link, $data['price']);
-            $product_desc = mysqli_real_escape_string($this->db->link, $data['product_desc']);
-            $type = mysqli_real_escape_string($this->db->link, $data['type']);
+            $customer_name = mysqli_real_escape_string($this->db->link, $data['customer_name']);
+            $phone_number = mysqli_real_escape_string($this->db->link, $data['phone_number']);
+            $city = mysqli_real_escape_string($this->db->link, $data['city']);
+            $district = mysqli_real_escape_string($this->db->link, $data['district']);
+            $address = mysqli_real_escape_string($this->db->link, $data['address']);
+            $session_id = session_id();
 
-
-            $permited = array('jpg','jpeg','png','jfif');
-            $file_name = $_FILES['image']['name'];
-            $file_size = $_FILES['image']['size'];
-            $file_temp = $_FILES['image']['tmp_name'];
-            
-            $div = explode('.',$file_name);
-            $file_ext = strtolower(end($div));
-            $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
-            $upload_image = "Uploads/".$unique_image;
-
-            if($file_size > 1000000){
-                echo "<span class = 'addError'>Image size should be less than 1MB</span> <br>";
-            }
-            elseif (in_array($file_ext,$permited) === false){
-                $alert = "<span class = 'addError'>You can up load only:-".implode(',',$permited)."</span> <br>" ;
-                return $alert;
-            }
-
-            move_uploaded_file($file_temp,$upload_image);
-            $query = "INSERT INTO tbl_product(product_name,category_id,price,product_desc,type,image) 
-                  VALUES('$product_name','$category_id','$price','$product_desc','$type','$unique_image')";
+            $query = "INSERT INTO tbl_customer_information(customer_name,phone_number,city,district,address,session_id) 
+                  VALUES('$customer_name','$phone_number','$city','$district','$address','$session_id')";
             $result = $this->db->insert($query);
-
-        
-            if($result)
-            {
-                $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1";
-                $result_product = $this->db->select($query)->fetch_assoc();
-
-                $product_id = $result_product['product_id'];
-                $file_name_desc = $_FILES['product_img_desc']['name'];
-                $file_tmp_desc = $_FILES['product_img_desc']['tmp_name'];
-                foreach($file_name_desc as $key => $value)
-                {
-                    move_uploaded_file($file_tmp_desc[$key], "Uploads_desc/".$value);
-                    $query = "INSERT INTO tbl_product_img_desc(product_id,product_img_desc) VALUES ('$product_id','$value')";
-                    $result_product_img_desc = $this->db->insert($query);
-                }
-
-                $alert = "<span class = 'addSuccess'>Add product successfully</span> <br>";
-                return $alert;
-            }
-                else{
-                $alert = "<span class = 'addError'>Can not add product</span> <br>";
-                return $alert;
-            }
+            header('Location:order-sucessful.php');
         }
         
     }
--- delivery.php
+++ delivery.php
@@ -1,50 +1,52 @@
 <?php
     include "Include_main/header.php";
+    include "Class/deliveryclass.php";
 ?>
 
 <?php
-    $product = new product();
+    $deliver = new delivery();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
 
-        $productAdd = $product->insert_product($_POST, $_FILES);
+        $customerInfo = $deliver->insert_customer_information($_POST);
     }
 ?>
+
 <!--DELIVERY AREA-->
 <section class="featured-product-area delivery-area">
         <div class="container">
             <div class="delivery-content row">
-                <div class="delivery-content-left">
-                    <form action="">
+                <div class="delivery-content-left col-lg-8 col-md-7 col-7">
+                    <form action="delivery.php" method="POST" enctype="multipart/form-data">
                         <div class="delivery-content-left-input-top row">
                             <div class="delivery-content-left-input-top-item">
                                 <label for="">Your Name<span style="color:red">*</span></label>
-                                <input type="text">
+                                <input type="text" name="customer_name">
                             </div>
                             <div class="delivery-content-left-input-top-item">
                                 <label for="">Phone Number<span style="color:red">*</span></label>
-                                <input type="text">
+                                <input type="text" name="phone_number">
                             </div>
                             <div class="delivery-content-left-input-top-item">
                                 <label for="">City/Province<span style="color:red">*</span></label>
-                                <input type="text">
+                                <input type="text" name="city">
                             </div>
                             <div class="delivery-content-left-input-top-item">
                                 <label for="">District<span style="color:red">*</span></label>
-                                <input type="text">
+                                <input type="text" name="district">
                             </div>
                         </div>
                         <div class="delivery-content-left-input-bottom row">
                             <label for="">Address<span style="color:red">*</span></label>
-                            <input type="text">
+                            <input type="text" name="address">
                         </div>
                         <div class="delivery-content-left-button row">
-                            <a href=""><span>&#10094;</span><p>Back to cart page</p> </a>
-                            <button><p style="font-weight: bold;">Submit order</p></button>
+                            <a href="cart.php"><span>&#10094;</span><p>Back to cart page</p> </a>
+                            <button type="submit" name="submit"><p style="font-weight: bold;">Submit order</p></button>
                         </div>
                     </form>
                 </div>
-                <div class="delivery-content-right">
+                <div class="delivery-content-right col-lg-4 col-md-5 col-5">
                     <table>
                         <tr>
                             <th>Product name</th>
