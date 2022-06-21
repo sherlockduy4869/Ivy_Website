a89a7dd7a7b18343532da4aba5cd0f862fc44ebb
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-21 21:53:10 +0700

    Update new root database

--- Config/config.php
+++ Config/config.php
@@ -1,6 +1,11 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "website_final_project");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "website_final_project");
+
+define("DB_HOST", "sql6.freesqldatabase.com");
+define("DB_USER", "sql6501250");
+define("DB_PASS", "ydtu3hg2qh");
+define("DB_NAME", "sql6501233");
 ?>
