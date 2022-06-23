da2e47e273b6ff90986db8cf30c3ed9278e8dbd2
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-23 15:27:46 +0700

    Change javascript to jquery

--- contactcheck.php
+++ contactcheck.php
@@ -1,8 +1,18 @@
 <?php
-$to = "duy.tran190201@vnuk.edu.vn";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
 
-mail($to,$subject,$txt,$headers);
+        $customer_name = $_POST['customer_name'];
+        $customer_email = $_POST['customer_email'];
+        $subject = $_POST['subject'];
+        $customer_message = $_POST['customer_message'];
+        $msg = wordwrap($customer_message, 70);
+
+        $mail_header = "From: ".$customer_name."<".$customer_email.">\r\n";
+        $recipient = "duy.tran190201@vnuk.edu.vn";
+
+        mail($recipient, $subject, $msg, $mail_header)
+        or die("Error!");
+
+        echo "message sent";
+    }
 ?>
