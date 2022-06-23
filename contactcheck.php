f8f186a7807833f4d813a79aa2a62ae5353bf8c1
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-23 14:30:29 +0700

    Test contactcheck.php

--- contactcheck.php
+++ contactcheck.php
@@ -1,17 +1,8 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+$to = "duy.tran190201@vnuk.edu.vn";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n";
 
-        $customer_name = $_POST['customer_name'];
-        $customer_email = $_POST['customer_email'];
-        $subject = $_POST['subject'];
-        $customer_message = $_POST['customer_message'];
-
-        $mail_header = "From: ".$customer_name."<".$customer_email.">\r\n";
-        $recipient = "duy.tran190201@vnuk.edu.vn";
-
-        mail($recipient, $subject, $customer_message, $mail_header)
-        or die("Error!");
-
-        echo "message sent";
-    }
+mail($to,$subject,$txt,$headers);
 ?>
