8e89c7fca2c900cb04fbf54133836321267afd72
unknown <nguyenquanghao1808@gmail.com>
2022-06-23 15:35:38 +0700

    update php file

--- about.php
+++ about.php
@@ -80,10 +80,10 @@
   </nav>
 
   <section>
-    <div class="header">
+  <div class="header">
       <h1 class="page-header">About Us</h1>
       <div class="line"></div>
-      <p class="sub-text">Top 1 Vietnamese Fashion</p>
+      <p class="sub-text">Products for every home</p>
     </div>
 
     <div class="row">
@@ -93,8 +93,8 @@
             src="https://ivymoda.com/assets/files/news/2022/03/08/a983fb0d282d71984fc750d7e5f60470.png"
             style="width: 80%">
           <div class="card-body">
-            <div class="card-title">IVYmoda Bla bla</div>
-            <div class="card-text">Một sản phẩm từ trên trời rơi xuống với chất lượng vô cùng đỉnk cout</div>
+            <div class="card-title">IVY moda</div>
+            <div class="card-text">"My Fashion Manifesto"</div>
           </div>
         </div>
       </div>
@@ -104,8 +104,8 @@
             src="https://ivymoda.com/assets/files/news/2022/03/08/5bb88418b3295127222ee87ebde52fa5.png"
             style="width: 81.3%">
           <div class="card-body">
-            <div class="card-title">IVYmoda Bla bla</div>
-            <div class="card-text">Một sản phẩm từ trên trời rơi xuống với chất lượng vô cùng đỉnk cout</div>
+            <div class="card-title">IVY men</div>
+            <div class="card-text">"Fashion of the Leader"</div>
           </div>
         </div>
       </div>
@@ -115,8 +115,8 @@
             src="https://ivymoda.com/assets/files/news/2022/03/08/350c3c97243d8d5c9bc24593155a56e0.png"
             style="width: 80%">
           <div class="card-body">
-            <div class="card-title">IVYmoda Bla bla</div>
-            <div class="card-text">Một sản phẩm từ trên trời rơi xuống với chất lượng vô cùng đỉnk cout</div>
+            <div class="card-title">IVY kids</div>
+            <div class="card-text">"Children must also dress well"</div>
           </div>
         </div>
       </div>
@@ -125,46 +125,83 @@
     <div class="row" style="background-color: #173c46">
       <div class="col-lg-12 mb-12">
         <div class="text-center">
-          <div class="core-value-header"> GIÁ TRỊ CỐT LÕI </div>
+          <div class="core-value-header"> CORE VALUES </div>
         </div>
       </div>
       <div class="col-lg-6 mb-4">
-        <div class="core-value">TÔN CHỈ<br>
-          "KHÁCH HÀNG LÀ TRUNG TÂM"</div>
+        <div class="core-value">CREDO<br>
+          "CUSTOMER IS CENTRAL"</div>
       </div>
       <div class="col-lg-6 mb-4">
         <div class="core-value-body">
-          <p>Với IVY moda, Khách hàng luôn là những celebs hạng A với những đặc quyền hấp dẫn nhất.</p>
+          <p>With IVY moda, customers are always A-class celebs with the most attractive privileges.</p>
         </div>
       </div>
       <div class="col-lg-6 mb-4">
-        <div class="core-value">TÔN CHỈ<br>
-          "KHÁCH HÀNG LÀ TRUNG TÂM"</div>
+        <div class="core-value">PASSION <br>
+          BE COMPLETE TO ACCEPT THE MISSION</div>
       </div>
       <div class="col-lg-6 mb-4">
         <div class="core-value-body">
-          <p>Với IVY moda, Khách hàng luôn là những celebs hạng A với những đặc quyền hấp dẫn nhất.</p>
+          <p>Bring a personal statement to the audience and customers.</p>
         </div>
       </div>
       <div class="col-lg-6 mb-4">
-        <div class="core-value">TÔN CHỈ<br>
-          "KHÁCH HÀNG LÀ TRUNG TÂM"</div>
+        <div class="core-value">INTEGRITY<br>
+          WITH YOURSELF AND ORGANIZATION</div>
       </div>
       <div class="col-lg-6 mb-4">
         <div class="core-value-body">
-          <p>Với IVY moda, Khách hàng luôn là những celebs hạng A với những đặc quyền hấp dẫn nhất.</p>
+          <p>"Words and Actions go hand in hand.
+            Do what you have said to be fair & reasonable"</p>
         </div>
       </div>
       <div class="col-lg-6 mb-4">
-        <div class="core-value">TÔN CHỈ<br>
-          "KHÁCH HÀNG LÀ TRUNG TÂM"</div>
+        <div class="core-value">PERSISTENCE WITH THE SAME MOTTO<br>
+          "CONTINUOUS STEP"</div>
       </div>
       <div class="col-lg-6 mb-4">
         <div class="core-value-body">
-          <p>Với IVY moda, Khách hàng luôn là những celebs hạng A với những đặc quyền hấp dẫn nhất.</p>
+          <p>Ready to face all difficulties and challenges to conquer new heights.</p>
         </div>
       </div>
+      
     </div>
+    <div class="main-text">
+      <br>
+      <h3>COMPANY RELEASE</h3>
+      <br>
+      <p>IVY moda has a development history spanning 15 years. Born in 2005, starting as an office fashion brand only for women, IVY moda has now completed its fashion "ecosystem" by launching product lines for both women and men. Men and children with distinct fashion statements. The brand has gradually transformed strongly and affirmed its leading position in fashion trends in the Vietnamese market
+
+        IVY moda is also the only mass fashion enterprise that has maintained 2 big fashion shows a year for the past 7 years. Strongly invested in huge costs and brains to treat fashionistas as well as loyal customers to high-class, lively, and full of fashion parties; highly appreciated by professionals and loved by customers.</p>
+
+      <br>
+
+      <p>IVY moda is also the only mass fashion enterprise that maintains 2 big fashion shows throughout the year
+        7 years ago. Strongly invest in huge costs and brains to entertain fans as well as loyal customers
+        design high-class, lively parties, full of fashion atmosphere; highly appreciated by experts
+        and loved by customers.</p>
+
+      <br>
+      <br>
+
+      <h3>CORPORATE CULTURE</h3>
+
+      <br>
+      
+      <p>IVY moda is the convergence of people who share the same passion for Vietnamese fashion dreams. IVY no
+        build culture from theory but always show through each action. We are proud to be a main team
+        Be honest, dare to say what you think and do what you say, persistently and persistently follow your dreams.</p>
+
+      <br>
+
+      <p>"If you want to go fast, go alone, if you want to go far, go together", 15 years of development is the best proof for the culture of Vietnam.
+        Perseverance, loyalty, creativity. Like the image of a sail in the wind on IVY's logo, we don't stop
+        desire to conquer, go beyond the waves to the sea regardless of hardships to explore the endless ocean".</p>
+
+    </div>
+    <div class = 'spacer'></div>
+
   </section>
 
 
