3df8f9621e69e7383aacaae914f809adf6e31b3b
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-24 22:06:37 +0700

    Change path root

--- Admin/Include/header.php
+++ Admin/Include/header.php
@@ -35,7 +35,7 @@
          ?>
         <div class="header-admin">
             <div class="header-admin-left">
-                <a href="index.php" class="vw-btn">Visit Website</a>
+                <a href="/index.php" class="vw-btn">Visit Website</a>
             </div>
             <div class="header-admin-right">
                 <h3>Hello <?php echo Session::get('adminName'); ?></h3>
