194b922882ecad0c333a0fc694ff9937d68d97ee
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-26 10:34:05 +0700

    Remove code javascript about slider

--- Resource/js/script.js
+++ Resource/js/script.js
@@ -21,41 +21,41 @@ $(document).ready(function(){
     )
     
     //Slider
-    const imgPosition = document.querySelectorAll(".aspect-ratio-169 img");
-    const imgContainer = document.querySelector(".aspect-ratio-169");
-    let index = 0;
-    let imgNumber = imgPosition.length;
-    const dotItem = document.querySelectorAll(".dot");
-    imgPosition.forEach(function(image, index){
-        image.style.left = index*100 + "%"
-        dotItem[index].addEventListener("click",function(){
-        slider(index);
-        })
-    })
+    // const imgPosition = document.querySelectorAll(".aspect-ratio-169 img");
+    // const imgContainer = document.querySelector(".aspect-ratio-169");
+    // let index = 0;
+    // let imgNumber = imgPosition.length;
+    // const dotItem = document.querySelectorAll(".dot");
+    // imgPosition.forEach(function(image, index){
+    //     image.style.left = index*100 + "%"
+    //     dotItem[index].addEventListener("click",function(){
+    //     slider(index);
+    //     })
+    // })
 
-    function imgSlide(){
-        index++;
-        if(index>=imgNumber){
-            index = 0;
-        }
-        slider(index);
-    }   
+    // function imgSlide(){
+    //     index++;
+    //     if(index>=imgNumber){
+    //         index = 0;
+    //     }
+    //     slider(index);
+    // }   
 
-    function slider(index){
-        imgContainer.style.left = "-" +index*100+ "%";
-        const dotActive = document.querySelector(".active");
-        dotActive.classList.remove("active");
-        dotItem[index].classList.add("active");
-    }
-    setInterval(imgSlide,5000)
+    // function slider(index){
+    //     imgContainer.style.left = "-" +index*100+ "%";
+    //     const dotActive = document.querySelector(".active");
+    //     dotActive.classList.remove("active");
+    //     dotItem[index].classList.add("active");
+    // }
+    // setInterval(imgSlide,5000)
 
-    const itemSliderbar = document.querySelectorAll(".category-left-li")
-    itemSliderbar.forEach(function(menu,index){
-        menu.addEventListener("click",function(){
-            menu.classList.toggle("block");
-        })
+    // const itemSliderbar = document.querySelectorAll(".category-left-li")
+    // itemSliderbar.forEach(function(menu,index){
+    //     menu.addEventListener("click",function(){
+    //         menu.classList.toggle("block");
+    //     })
         
-    })
+    // })
 
     //Slick Slider
 
