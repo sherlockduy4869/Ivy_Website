77039a78c3220dcaf5fbc900e87f7fd88dd862c5
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-29 11:58:13 +0700

    Change database for project

--- Config/config.php
+++ Config/config.php
@@ -4,8 +4,8 @@
 // define("DB_PASS", "");
 // define("DB_NAME", "website_final_project");
 
-define("DB_HOST", "sql6.freesqldatabase.com");
-define("DB_USER", "sql6501250");
+define("DB_HOST", "fdb33.awardspace.net");
+define("DB_USER", "4089761_ivy");
 define("DB_PASS", "ydtu3hg2qh");
-define("DB_NAME", "sql6501250");
+define("DB_NAME", "4089761_ivy");
 ?>
