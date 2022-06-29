60717d2c5079721447d1277db6d558d37dfeb4c5
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-29 10:53:40 +0700

    Test project

--- Include_main/header.php
+++ Include_main/header.php
@@ -2,7 +2,7 @@
     // ob_start();
     //include_once $_SERVER['DOCUMENT_ROOT']."/Class/category.php";
     //include_once $_SERVER['DOCUMENT_ROOT'].'/Lib/session.php';
-    Session::init();
+    //Session::init();
     // $cate = new category();
     // $cate_dropdown = $cate->show_category_list();
 ?>
