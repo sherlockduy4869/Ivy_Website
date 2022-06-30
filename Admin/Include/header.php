90a9897a4393e13d697e30bfe167cc23c2abce35
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-30 08:44:18 +0700

    Adding data table for admin page

--- Admin/Include/header.php
+++ Admin/Include/header.php
@@ -12,13 +12,20 @@
     <title>Administrator</title>
     <!-- CSS -->
     <link rel="stylesheet" href="./css/styleAdmin.css">
+
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
     <!-- CkFinder -->
     <script src="https://cdn.ckeditor.com/4.18.0/standard/ckeditor.js"></script>
     <script src="../ckfinder/ckfinder.js"></script>
+
     <!-- Jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Jquery/Javascript -->
+    <script src="/Admin/js/scriptAdmin.js"></script>
+
     <!-- DataTables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
